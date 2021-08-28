b9c7fbd51cb3626f89880846016cd8c285025de8
igorunderplayer <igorschug6@gmail.com>
2021-08-27 23:13:13 -0300

    Typescript

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../styles/Home.module.css'
+
+const Home: React.FC = () =>  {
+  return (
+    <div className={styles.container}>
+      oi
+    </div>
+  )
+}
+
+export default Home
