9f4951c2a8e0be3f079bedd18d703a1596b78298
igorunderplayer <igorschug6@gmail.com>
2021-08-27 23:18:57 -0300

    Add dependencies

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from '../styles/Home.module.css'
 
+export interface IUser {
+
+}
+
 const Home: React.FC = () =>  {
+  const [user, setUser] = useState<IUser>()
   return (
     <div className={styles.container}>
-      oi
+    
     </div>
   )
 }
